3ad2d8dd1dd9c14cdff3f63e100a0d18395a8793
Jose Aguilar Quesada <jmaq.cr@gmail.com>
2016-09-24 16:51:16 -0600

    PROCEDURE UPDATE

--- database/stored-procedures.sql
+++ database/stored-procedures.sql
@@ -1,30 +1,67 @@
--- Volcando estructura para procedimiento programathon2016.insertPYME
-DELIMITER //
-CREATE DEFINER=`root`@`localhost` PROCEDURE `insertPYME`(IN `NombreComercio` CHAR(100), IN `Estado` INT, IN `Sector` INT, IN `CedulaJuridica` VARCHAR(50), IN `GeneroPropietario` INT, IN `Telefono` CHAR(50), IN `EsActiva` BIT(1), IN `EsNegocioFamiliar` BIT(1), IN `Logo` LONGTEXT, IN `AnnioInicioOperaciones` TINYINT(4))
-BEGIN
-INSERT into pyme(pyme.NombreComercio, pyme.EstadoID, pyme.SectorID, pyme.CedJuridica,
-pyme.GeneroPropietarioID, pyme.NumeroTelefono, pyme.EsActiva, pyme.EsNegocioFamiliar,
-pyme.Logo, pyme.AnnoInicioOperaciones) values
-(NombreComercio, Estado, Sector, CedulaJuridica, GeneroPropietario, Telefono,EsActiva, EsNegocioFamiliar, Logo, AnnioInicioOperaciones);
-
-END//
-DELIMITER ;
-
-
--- Volcando estructura para procedimiento programathon2016.getPaises
-DELIMITER //
+DELIMITER $$
 CREATE DEFINER=`root`@`localhost` PROCEDURE `getPaises`()
 BEGIN
 	Select pais.Nombre from pais;
-END//
+END$$
 DELIMITER ;
 
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `insertPYME`(
+	IN `pNombreComercio` CHAR(100),
+	IN `pEstadoID` INT(11),
+	IN `pSectorID` INT(11),
+	IN `pAnnoInicioOperaciones` INT(4),
+	IN `pNumeroTelefono` CHAR(50),
+	IN `pDireccion` CHAR(200),
+	IN `pEsActiva` BIT(1),
+	IN `pEsNegocioFamiliar` BIT(1),
+	IN `pLogo` BLOB,
+	IN `pExtensionLogo` CHAR(3),
+	IN `pFechaCreacion` DATETIME,
+	IN `pFechaUltimaActualizacion` DATETIME,
+	IN `pEsFacebookAppInstalado` BIT(1),
+	IN `pUsuarioID` INT(11),
+	IN `pGeneroPropietarioID` INT(11),
+	IN `pCedJuridica` VARCHAR(50)
+)
+    DETERMINISTIC
+BEGIN
+START TRANSACTION;
+insert into pyme
+values (pNombreComercio,pEstadoID,pSectorID,pAnnoInicioOperaciones,pNumeroTelefono,pDireccion,pEsActiva,pEsNegocioFamiliar,pLogo,pExtensionLogo,pFechaCreacion,pFechaUltimaActualizacion,pEsFacebookAppInstalado,pUsuarioID,pGeneroPropietarioID,pCedJuridica); 
+COMMIT;
 
--- Volcando estructura para procedimiento programathon2016.insertUsuario
-DELIMITER //
+
+END$$
+DELIMITER ;
+
+DELIMITER $$
 CREATE DEFINER=`root`@`localhost` PROCEDURE `insertUsuario`(IN `NombreUsuario` VARCHAR(50), IN `NombreCompleto` VARCHAR(100), IN `Clave` VARCHAR(50), IN `EmailContacto` VARCHAR(50))
 BEGIN
 INSERT INTO usuario(usuario.Usuario,usuario.NombreCompleto,usuario.Clave, usuario.EmailContacto) values
 (NombreUsuario,NombreCompleto,Clave,EmailContacto);
-END//
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `insertRespuestas`(
+	IN `pRespuesta01` INT(11),
+	IN `pRespuesta02` INT(11),
+	IN `pRespuesta03` INT(11),
+	IN `pRespuesta04` INT(11),
+	IN `pRespuesta05` INT(11),
+	IN `pFechaRespuesta` DATETIME,
+	IN `pGeneroID` VARCHAR(1),
+	IN `pCampo1` VARCHAR(100),
+	IN `pCampo2` VARCHAR(100),
+	IN `pRangoEdad` TINYINT(1),
+	IN `pPymeID` INT(11)
+)
+    DETERMINISTIC
+BEGIN
+START TRANSACTION;
+insert into respuesta
+values(pRespuesta01,pRespuesta02,pRespuesta03,pRespuesta04,pRespuesta05,pFechaRespuesta,pGeneroID,pCampo1,pCampo2,pRangoEdad,pPymeID);
+COMMIT;
+END$$
 DELIMITER ;
