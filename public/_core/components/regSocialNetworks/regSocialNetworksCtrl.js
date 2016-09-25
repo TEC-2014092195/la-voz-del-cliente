fabffff2256a118de87acddc22c4b88177e82c20
Jose Aguilar Quesada <jmaq.cr@gmail.com>
2016-09-24 21:57:04 -0600

    regSocial y regUserData

--- public/_core/components/regSocialNetworks/regSocialNetworksCtrl.js
+++ public/_core/components/regSocialNetworks/regSocialNetworksCtrl.js
@@ -1,5 +1,15 @@
 app.controller('regSocialNetworksCtrl', function ($scope, $timeout, $location) {
+    $scope.resultado = {
+        facebookDir: "",
+        twitterDir: "",
+        youtubeDir: "",
+        siteDir: "",
+        mailDir: ""
+    }
 
+    $scope.submit = function () {
+        console.log($scope.resultado);
+    }
 });
 
 
--- public/_core/components/regUserData/regUserDataCtrl.js
+++ public/_core/components/regUserData/regUserDataCtrl.js
@@ -1,6 +1,20 @@
 app.controller('regUserDataCtrl', function ($scope, $timeout, $location) {
+    $scope.resultado = {
+        nombrecompleto: "",
+        nombreusuario: "",
+        clave: "",
+        reclave: ""
+    }
 
+    $scope.submit = function () {
+        if($scope.resultado.clave != $scope.resultado.reclave){
+            alert("Las claves no coinciden");
+            $scope.resultado.clave = "";
+            $scope.resultado.reclave = "";
+        }
+        else{
+            console.log($scope.resultado);
+        }
 
-
-
+    }
 });
